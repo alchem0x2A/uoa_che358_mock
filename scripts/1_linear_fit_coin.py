8bdd299289bd0a67727af009306be903a3f59e26
T.Tian <alchem0x2a@gmail.com>
2023-06-19 12:02:20 -0400

    add more figures

--- scripts/1_linear_fit_coin.py
+++ scripts/1_linear_fit_coin.py
@@ -8,17 +8,19 @@ data = np.load("result_coin.npz", allow_pickle=True)
 r, p, y = data["r"], data["p"], data["flip"]
 
 def plot_main():
-    fig, ax = plt.subplots(figsize=(8, 6))
+    fig, axes = plt.subplots(1, 2, figsize=(10, 4))
 
+    ax, ax2 = axes[0], axes[1]
+    
     ax.set_xlim(0, 1)
-    ax.set_ylim(-0.1, 1.1)
-    ax.set_yticks([0, 1])
+    ax.set_ylim(-0.3, 1.3)
+    # ax.set_yticks([0, 1])
     ax.axhline(y=1.0, ls="--", color="grey", lw=1.5)
     ax.axhline(y=0.0, ls="--", color="grey", lw=1.5)
-    ax.set_xlabel("$r$")
-    ax.set_ylabel("$y$")
-    ax.plot(r[y == 1], y[y == 1], "o", alpha=0.2 )
-    ax.plot(r[y == 0], y[y == 0], "o", alpha=0.2 )
+    ax.set_xlabel("$x$ (head weight ratio)")
+    ax.set_ylabel("$y$ (head or tail)")
+    l1, = ax.plot(r[y == 1], y[y == 1], "o", alpha=0.2 )
+    l2, = ax.plot(r[y == 0], y[y == 0], "o", alpha=0.2 )
 
     fit = np.polyfit(r, y, deg=1)
     fit_fun = np.poly1d(fit)
@@ -27,18 +29,36 @@ def plot_main():
     y_fit = fit_fun(r)
     r2 = r2_score(y, y_fit)
     print(f"R^2: {r2}")
-    ax.plot(xx, yy, lw=2.0, color="#7f5c00")
+    ax.fill_between([0, 1], 1.0, 1.3, color=l1.get_c(), alpha=0.5)
+    ax.fill_between([0, 1], -0.3, 0, color=l2.get_c(), alpha=0.5)
+    ax.plot(xx, yy, lw=3.0, color="#7f5c00")
+    ax.text(x=0.5, y=fit_fun(0.5) - 0.08,
+            s=f"$p = {{{fit[1]:.2f}}} + {{{fit[0]:.2f}}}x$")
+    ax.text(x=0.5, y=fit_fun(0.5) - 0.20,
+            s=f"$R^2 = {{{r2:.2f}}}$")
+    ax.text(x=0.5, y=1.15,
+            va="center",
+            ha="center",
+            style="italic",
+            s="Invalid Region")
+    ax.text(x=0.5, y=-0.15,
+            va="center",
+            ha="center",
+            style="italic",
+            s="Invalid Region")
     # print(xx, yy)
-    fig.savefig("coin_linear_fit.pdf")
+    # fig.tight_layout()
+    # fig.savefig("coin_linear_fit.pdf")
 
     residuals = y - y_fit
-    fig, ax = plt.subplots(figsize=(4, 4))
     # ax.hist(residuals, bins=20)
-    ax.plot(y_fit, residuals, "o")
-    ax.set_xlabel("Fitted value")
-    ax.set_ylabel("Residuals")
+    ax2.plot(y_fit, residuals, "o", color="grey")
+    ax2.set_xlabel("Predicted values")
+    ax2.set_ylabel("Residuals")
+
     fig.tight_layout()
-    fig.savefig("coin_linear_res.pdf")
+    
+    fig.savefig("coin_linear_combined.pdf")
     
     return
 
